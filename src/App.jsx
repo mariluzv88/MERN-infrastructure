06332ccb76698ee3d047b4e464704cff9700815e
mariluzv88 <59103912+mariluzv88@users.noreply.github.com>
2023-08-29 16:04:21 -0400

    MERN-Stack Infrastructure

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import { getUser } from './utilities/users-service';
+import {Route, Routes} from 'react-router-dom'
+import AuthPage from './pages/Auth/AuthPage';
+import NewOrderPage from './pages/NewOrder/NewOrderPage';
+import OrderHistoryPage from './pages/OrderHistory/OrderHistoryPage';
+import NavBar from './components/Nav/NavBar';
+import './App.css';
+
+function App() {
+  const [user,setUser]=useState(getUser())
+ 
+  return (
+    <main className="App">
+     {
+       user?
+       <>
+          <NavBar user={user} setUser={setUser}/>
+         
+          <Routes>
+            <Route path='/orders/new' element={<NewOrderPage/>}/>
+            <Route path='/orders' element={<OrderHistoryPage/>}/>
+          </Routes>
+       </>
+       :
+      <AuthPage setUser={setUser}/>
+     }
+    </main>
+  );
+}
+
+export default App;
